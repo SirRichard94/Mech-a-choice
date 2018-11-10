e6206f0775c250fa3686688dfc40dd0ed7c1e337
Ricardo Rodriguez <sirrichard94@gmail.com>
2018-11-10 09:57:39 -0800

    Changed menu for buttons, made theme

--- GUI/Menu.gd
+++ GUI/Menu.gd
@@ -1,6 +1,5 @@
 extends Control
-
-var MenuItemPrefab = preload("res://GUI/MenuItem.tscn")
+var MenuItemTheme = preload("res://GUI/Menu Items/menu_theme.tres")
 
 var menu_items = []
 var title_text = "HERO CHOOSES BRAVE ACTION !"
@@ -8,45 +7,25 @@ var title_text = "HERO CHOOSES BRAVE ACTION !"
 onready var title_label = $VBoxContainer/Title/MarginContainer/Label
 onready var item_container = $"VBoxContainer/Rect/MarginContainer/HBoxContainer/ScrollContainer/Menu Items"
 
-var i_selected = 0
-
 var menu_height = 250
-var tween_time = 1.0
+var tween_time = 0.7
 
 func _ready():
 	$EnterTween.interpolate_property(self, "margin_top", margin_bottom, -menu_height, tween_time, Tween.TRANS_BOUNCE, Tween.EASE_OUT )
 	$ExitTween.interpolate_property(self, "margin_top",-menu_height, margin_bottom, tween_time, Tween.TRANS_EXPO, Tween.EASE_OUT )
-	refresh_items()
-	
-	
-	appear()
 
 func appear():
+	visible = true
 	$EnterTween.start()
+	yield($EnterTween,"tween_completed") #continue when completed
+	item_container.get_child(0).grab_focus()
 
 func disappear():
 	$ExitTween.start()
-	#yield($ExitTween, "tween_completed") #wait until completed tween
+	release_focus()
+	yield($ExitTween,"tween_completed") #continue when completed
+	visible = false
 
-
-func _input(event):
-	if event.is_action_pressed("ui_accept"):
-		if menu_items[i_selected].has_method("doit_faggot"):
-			menu_items[i_selected].doit_faggot()
-			disappear()
-		
-	if event.is_action_pressed("ui_down"):			
-		
-		item_container.get_child(i_selected).get_node("Cursor").visible = false
-		i_selected = (i_selected +1) % menu_items.size()
-		item_container.get_child(i_selected).get_node("Cursor").visible = true
-		
-	if event.is_action_pressed("ui_up"):
-		item_container.get_child(i_selected).get_node("Cursor").visible = false
-		i_selected = (max(i_selected-1,0)) % menu_items.size()
-		item_container.get_child(i_selected).get_node("Cursor").visible = true
-	
-	
 func refresh_items():
 	title_label.text = title_text
 	
@@ -54,8 +33,12 @@ func refresh_items():
 		child.queue_free()
 	
 	for item in menu_items: #load items
-		var m_item = MenuItemPrefab.instance()
-		m_item.get_node("Label").text = item.name
-		m_item.get_node("Label").name = item.name
-		#m_item.reference = item
+		var m_item = ToolButton.new()
+		m_item.text = item.name
+		m_item.theme = MenuItemTheme
+		m_item.align = Button.ALIGN_LEFT
+		
+		m_item.connect("pressed", item, "doit_faggot")
+		m_item.connect("pressed", self, "disappear")
+		
 		item_container.add_child(m_item)
--- Player/Player.gd
+++ Player/Player.gd
@@ -5,4 +5,7 @@ onready var menu_panel = get_node("../GUI/VBoxContainer/Container/Menu Panel")
 func _ready():
 	var actions = $Actions.get_children()
 	menu_panel.menu_items = actions
-	menu_panel.call_deferred("refresh_items")
+	
+	yield(get_tree(),"idle_frame")
+	menu_panel.refresh_items()
+	menu_panel.appear()
