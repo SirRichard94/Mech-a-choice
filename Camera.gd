1992bc8eb756d4aadd5dc7bc813c2e01da10593a
Ricardo Rodriguez <sirrichard94@gmail.com>
2018-11-11 17:10:28 -0800

    added screen shake better

--- Camera.gd
+++ Camera.gd
@@ -1,14 +1,19 @@
 extends Node2D
 
-export var shake_x = 100.0
-export var shake_y = 70
+export var shake_x = 250.0
+export var shake_y = 300.0
+export var max_shake_time = 2.0
+export var max_rotation = .04
 
-var trauma = 1
+var trauma = 0
 
+func add_shake(f):
+	trauma+=f
 
 func _process(delta):
-	var new_trauma = trauma - trauma*0.5 *delta
-	trauma = max(0,new_trauma)
+	var new_trauma =  trauma - delta/max_shake_time
+	trauma = clamp(new_trauma,0,1.0)
 	
-	$Camera2D.position.x = position.x + trauma * randf() * shake_x
-	$Camera2D.position.y = position.y + trauma * randf() * shake_y
+	$Camera2D.position.x = pow(trauma,3) * (randf()*2 -1 ) * shake_x
+	$Camera2D.position.y = pow(trauma,3) * (randf()*2 -1 ) * shake_y
+	$Camera2D.rotation = PI * pow(trauma,3) * (randf()*2 -1 ) * max_rotation
