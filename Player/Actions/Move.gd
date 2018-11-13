ba5e4441fc96b51055f40f10f80c54dc1428a681
Ricardo Rodriguez <sirrichard94@gmail.com>
2018-11-12 23:47:27 -0800

    Improved Move Action, now you can return from submenu, and can be disabled

--- Player/Actions/Move.gd
+++ Player/Actions/Move.gd
@@ -1,16 +1,18 @@
 extends "res://Player/Actions/ActionItem.gd"
 
-var MoveLocationScene = preload("res://Player/Actions/MoveLocation.tscn")
-
+var MoveToAction = preload("res://Player/Actions/MoveLocation.tscn")
+var BackAction = preload("res://Player/Actions/Back.tscn")
 
 
 func _ready():
 	description = "Skidaddle out of here"
 
+func get_paths():
+	var area = player.get_node("AreaComponent").get_area()
+	return area.get_paths()
 
 func _do_action():
-	var area = player.get_node("AreaComponent").get_area()
-	var paths = area.get_paths()
+	var paths = get_paths()
 	
 	if paths.empty():
 		description = "Nowhere to Go"
@@ -21,8 +23,9 @@ func _do_action():
 	
 	yield(get_tree(), "idle_frame")
 	
+	add_child(BackAction.instance())
 	for path in paths:
-		var move_to = MoveLocationScene.instance()
+		var move_to = MoveToAction.instance()
 		move_to.target = path
 		move_to.name = path.name
 		
@@ -32,4 +35,6 @@ func _do_action():
 	
 	gui.action_menu.title_text = "HERO ON THE MOVE"
 	gui.action_menu.menu_items = get_children()
-	
+
+func is_disabled():
+	return get_paths().empty()
